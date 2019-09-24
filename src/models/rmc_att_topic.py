updated the model with the possibility to use or not the topic, correct bug in the KL divergence that could be a measure to compute how the topic works

src/models/rmc_att_topic.py
@@ -34,13 +34,13 @@ def generator(x_real, temperature, x_topic, vocab_size, batch_size, seq_len, gen
 
     def _gen_recurrence(i, x_t, h_tm1, gen_o, gen_x, gen_x_onehot_adv, lambda_values, gen_x_no_lambda):
         mem_o_t, h_t = gen_mem(x_t, h_tm1)  # hidden_memory_tuple, output della memoria che si potrebbe riutilizzare
-        if kwargs["TopicInMemory"]:
+        if kwargs["TopicInMemory"] and not kwargs["NoTopic"]:
             mem_o_t, h_t = gen_mem(g_topic_embedding(x_topic), h_t)
         o_t = g_output_unit(mem_o_t)  # batch x vocab, logits not prob
 
         # print_op = tf.print("o_t shape", o_t.shape, ", o_t: ", o_t[0], output_stream=sys.stderr)
 
-        if not kwargs["TopicInMemory"]:
+        if not kwargs["TopicInMemory"] and not kwargs["NoTopic"]:
             topic_vector = x_topic
             lambda_param = g_output_unit_lambda(mem_o_t)
             # print_op_lambda = tf.print("Lambda= iteration:", i, " shape: {}, values:".format(lambda_param.shape), lambda_param)
@@ -111,10 +111,10 @@ def generator(x_real, temperature, x_topic, vocab_size, batch_size, seq_len, gen
 
     def _pretrain_recurrence(i, x_t, h_tm1, g_predictions):
         mem_o_t, h_t = gen_mem(x_t, h_tm1)
-        if kwargs["TopicInMemory"]:
+        if kwargs["TopicInMemory"] and not kwargs["NoTopic"]:
             mem_o_t, h_t = gen_mem(g_topic_embedding(x_topic), h_t)
         o_t = g_output_unit(mem_o_t)
-        if not kwargs["TopicInMemory"]:
+        if not kwargs["TopicInMemory"] and not kwargs["NoTopic"]:
             lambda_param = g_output_unit_lambda(mem_o_t)
             o_t = o_t + lambda_param * x_topic
         g_predictions = g_predictions.write(i, tf.nn.softmax(o_t))  # batch_size x vocab_size
@@ -217,6 +217,7 @@ def topic_discriminator(x_onehot, x_topic, batch_size, seq_len, vocab_size, dis_
 
     logits = linear(flatten, output_size=1, use_bias=True, sn=sn, scope='fc_topic')
     logits = tf.squeeze(logits, -1)  # batch_size
+    logits = tf.sigmoid(logits)
     return logits
 
 

src/real/real_gan/real_loader.py
@@ -6,7 +6,7 @@ import gc
 import numpy as np
 
 from path_resolution import resources_path
-from topic_modelling.lda_utils import get_perc_sent_topic, process_texts
+from topic_modelling.lda_utils import get_perc_sent_topic, process_texts, get_perc_few_sent_topic
 from topic_modelling.lda_topic import train_specific_LDA, get_corpus
 
 
@@ -114,7 +114,6 @@ class RealDataTopicLoader(RealDataLoader):
         self.topic_batches = np.split(np.array(self.sentence_topic_array), self.num_batch, axis=0)
 
         self.pointer = 0
-        del self.lda
 
     def next_batch(self, only_text=True):
         # with the parameter I can change only when needed
@@ -207,10 +206,7 @@ class RealDataTopicLoader(RealDataLoader):
     def get_model_index(self, lda_index) -> List[int]:
         word = self.lda_index_word_dict[lda_index]
         from_lemmatize = [text_index for text_index in range(len(self.texts)) if self.texts[text_index] == word]
-        try:
-            return [int(self.model_word_index_dict[word])] + from_lemmatize
-        except KeyError:
-            return from_lemmatize
+        return from_lemmatize
 
     def get_LDA(self, word_index_dict, index_word_dict, data_file):
         self.vocab_size = len(self.model_index_word_dict)
@@ -245,18 +241,16 @@ class RealDataTopicLoader(RealDataLoader):
         t = time.time()
         tmp = process_texts(sentences, self.lda.stops)
         corpus_bow = [self.lda.dictionary.doc2bow(i) for i in tmp]
-        df = get_perc_sent_topic(ldamodel=self.lda.lda_model, corpus=corpus_bow, texts=sentences,
-                                 topic_num=self.topic_num)
+        df = get_perc_few_sent_topic(ldamodel=self.lda.lda_model, corpus_bow=corpus_bow, topic_num=self.topic_num)
         topic_weights = df.values[:, 1:self.topic_num + 1]  # num_sentences x num_topic (each row sum to 1)
-        topic_sentences = np.dot(topic_weights, self.topic_matrix)  # num_sentences x num_word
-        topic_sentences = np.divide(topic_sentences,
-                                    np.sum(topic_sentences, axis=1, keepdims=True))  # rowwise normalization
+        print(df.head(5))
+        topic_sentences = np.dot(topic_weights, self.topic_matrix)  # num_sentences x num_lda_word
+
 
         real_vector = np.zeros(
             (topic_sentences.shape[0], len(self.model_word_index_dict) + 1))  # sentence_number x vocab_size
 
         for ind, invere_index in enumerate(self.inverse_indexes):
-            # more than one index in the model because of lemmatization
             for x in invere_index:
                 real_vector[:, x] = topic_sentences[:, ind]
         gc.collect()

src/topic_modelling/lda_utils.py
@@ -164,8 +164,25 @@ def word_cloud(lda):
     plt.show()
 
 
-def get_perc_sent_topic(lda, topic_num, data_file):
+def get_perc_few_sent_topic(ldamodel, corpus_bow, topic_num):
+    sent_topics_df = pd.DataFrame()
 
+    # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus_bow]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list
+        to_append = np.zeros(topic_num)
+        for j, (topic_n, prop_topic) in enumerate(row):
+            to_append[topic_n] = prop_topic
+        sent_topics_df = sent_topics_df.append(pd.Series(to_append), ignore_index=True)
+
+    sent_topics_df.columns = ["Topic {}".format(topic_number) for topic_number in range(topic_num)]
+
+    # Add original text to the end of the output
+    sent_topics_df = sent_topics_df.reset_index()
+    return sent_topics_df
+
+
+def get_perc_sent_topic(lda, topic_num, data_file):
     file_path = "{}-{}-{}".format(lda.lda_model, topic_num, data_file[-10:])
     file_path = resources_path("topic_models", file_path)
     try: